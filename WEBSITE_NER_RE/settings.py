3de31eab17dcaad3944a8159d1d99c1805daa478
Dimas Dwi Putra <61466102+Dimas263@users.noreply.github.com>
2022-07-19 08:55:00 +0700

    update

--- WEBSITE_NER_RE/settings.py
+++ WEBSITE_NER_RE/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s*t+-ys%044bbf#fw@xzrl!1q_q(yr35dk4w3)(@!9af_)v_+s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
